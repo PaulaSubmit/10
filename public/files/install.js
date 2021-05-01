e44a13b65bf91389eee3da9b12174a668927723b
PaulaSubmit <83514958+PaulaSubmit@users.noreply.github.com>
2021-05-02 00:26:54 +0200

    Add files via upload

---
+++ public/files/install.js
@@ -0,0 +1,31 @@
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+function saveBeforeInstallPromptEvent(evt){
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+}
+
+function installPWA(evt) {
+   
+    deferredInstallPrompt.prompt();
+evt.srcElement.setAttribute('hidden', true);
+
+    deferredInstallPrompt.userChoice.then((choice)=> {
+        if(choice.outcome === 'accepted') {
+            console.log("aceptado", choice);
+        }
+        else {
+            console.log("No aceptado", choice);
+        }
+        deferredInstallPrompt = null;
+    });
+}
+window.addEventListener('appinstalled', logAppInstalled);
+
+function logAppInstalled(evt){
+console.log("Juego instalado app", evt);
+
+}
